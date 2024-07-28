2a2143529545e88579673c8986bcc2b987647b06
susanyazdan1382 <160869242+susanyazdan1382@users.noreply.github.com>
2024-07-29 00:06:31 +0430

    updates index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 
 
-
 document.getElementById('calculate').addEventListener('click', function() {
     var num1 = parseInt(document.getElementById('num1').value);
     var num2 = parseInt(document.getElementById('num2').value);
